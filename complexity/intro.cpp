2950cd41b0ca85bc10cf865126306640de392625
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-28 09:56:57 +0530

    compare_function

--- complexity/intro.cpp
+++ complexity/intro.cpp
@@ -4,7 +4,11 @@
 #include<algorithm>
 using namespace std;
 
-int main(){
+bool compare(int a, int b){
+    return a>b;
+}
+
+int main(){ 
 #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
