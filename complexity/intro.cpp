d2eb123bb391de0cad8b4026e55a11b795c8bc1b
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-28 09:40:44 +0530

    file_template_update

---
+++ complexity/intro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<ctime>
+#include<algorithm>
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+}
