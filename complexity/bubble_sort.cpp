e56690c2491adc6c104c855343f17f374cc05bf1
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-28 10:08:12 +0530

    simple_update

--- complexity/bubble_sort.cpp
+++ complexity/bubble_sort.cpp
@@ -8,11 +8,11 @@ bool compare(int a, int b){
     return a>b;
 }
 
-void hubble_sort(int a[], int n){
+void bubble_sort(vector<int> &arr, int n){
     for(int times=1;times<=n-1;times++){
         for(int j=0;j<=n-times-1;j++){
-            if(compare(a[j],a[j+1])){
-                swap(a[j],a[j+1]);
+            if(compare(arr[j],arr[j+1])){
+                swap(arr[j],arr[j+1]);
             }
         }
     }
@@ -25,11 +25,15 @@ int main(){
     freopen("error.txt","w",stderr);
 #endif
 
-    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
-    int n = sizeof(arr)/sizeof(int);
+    int n;
+    cin>>n;
+    vector<int> arr(n,0);
+    for(int i=0;i<n;i++){
+        arr[i]=n-i;
+    }
 
     auto start_time = clock();
-    
+    bubble_sort(arr,n);
     auto end_time = clock();
 
     cout<<end_time - start_time<<endl;
