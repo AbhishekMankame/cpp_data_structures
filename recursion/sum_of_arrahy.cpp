08e8fe5b8196f5c170afe7c65776d81936bba1a2
AbhishekMankame <abhishekmankame962@gmail.com>
2023-03-29 20:32:22 +0530

    simple_update

--- recursion/sum_of_arrahy.cpp
+++ recursion/sum_of_arrahy.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // sum of array
 // sum(n,a) -> sum of elements in a uptill n index
+// sum(n,a) = a[n] + sum(n-1,a)
 
 void fast(){
     ios_base::sync_with_stdio(0); 
@@ -11,7 +12,9 @@ void fast(){
     cout.tie(0);
 }
 
-int sum(int n){}
+int sum(int n){
+    if(n==0) return 1;
+}
 
 
 int main(){
