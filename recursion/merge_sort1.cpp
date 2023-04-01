623d6bb7932c33f3be9f7e82a44f7a343775a966
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-01 21:00:30 +0530

    code_update

--- recursion/merge_sort1.cpp
+++ recursion/merge_sort1.cpp
@@ -53,9 +53,17 @@ int main(){
 
     fast();
     int t=1;
-    cin>>t;
+    //cin>>t;
     while(t--){
-
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        mergeSort(0,n-1);
+        for(int i=0;i<n;i++){
+            cout<<a[i]<<" ";
+        }
     }
 
     return 0;
