5c68f537462962af52364969cd02cf72800c4908
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-01 20:35:58 +0530

    simple_update

--- recursion/merge_sort1.cpp
+++ recursion/merge_sort1.cpp
@@ -13,6 +13,15 @@ void merge(int l, int r, int mid){
     int l_sz = mid-l+1;
     int L[l_sz+1];
     int r_sz = r - mid; // r_sz = r - (mid + 1) + 1
+    int R[r_sz+1];
+    for(int i=0;i<l_sz;i++){
+        L[i] = a[i+l];
+    }
+    for(int i=0;i<r_sz;i++){
+        R[i]=a[i+mid+1];
+    }
+    L[l_sz] = R[r_sz] = INT_MAX;
+
 }
 
 int main(){
