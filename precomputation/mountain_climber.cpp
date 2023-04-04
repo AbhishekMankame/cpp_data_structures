62f894947eb83d95504ce71d0a758b0e35b95433
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-04 08:06:54 +0530

    code_update

--- precomputation/mountain_climber.cpp
+++ precomputation/mountain_climber.cpp
@@ -1,1 +1,25 @@
 // mountain climbers ---> codeforces problem
+
+#include <bits/stdc++.h>
+using namespace std;
+int t;
+string s;
+int main () {
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> s;
+        int brn = 0, u = 0;
+        for (int j = 0; j < s.size(); j++) {
+            if (s[j] == 'l' || s[j] == 'd' || s[j] == 'f' || s[j] == 'b' || s[j] == 'h' || s[j] == 'k' || s[j] == 't')
+                brn++;
+            if (s[j] == 'q' || s[j] == 'g' || s[j] == 'j' || s[j] == 'p' || s[j] == 'y')
+                brn--;
+            if (brn < 0) u = 1;
+        }
+        if (brn == 0 && u == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
