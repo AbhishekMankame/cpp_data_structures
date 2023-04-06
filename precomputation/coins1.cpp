c24675913dc8701ecd58edb9bd8dd5aaa74c35c8
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-06 22:53:51 +0530

    err_update

--- precomputation/coins1.cpp
+++ precomputation/coins1.cpp
@@ -8,7 +8,8 @@ int main() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
-    
+    freopen("error.txt","w",stderr);
+
 #endif
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
