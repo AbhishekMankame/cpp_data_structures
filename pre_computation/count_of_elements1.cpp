87056ed0b6a51c7418f46a2efb817b35b8ca6741
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-07 14:41:56 +0530

    simple_update

---
+++ pre_computation/count_of_elements1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+}
