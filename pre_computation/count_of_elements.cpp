48f71f51d949448d1343c9e75850e1c520924501
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-07 12:49:09 +0530

    details_update

--- pre_computation/count_of_elements.cpp
+++ pre_computation/count_of_elements.cpp
@@ -1,3 +1,12 @@
+/*
+Given array a of N integers. Given Q queires and in each query given a number X, print counnt of that number in array.
+
+Constraints
+1 <= N <= 10^5
+1 <= a[i] <= 10^7
+1 <= Q <= 10^5
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
