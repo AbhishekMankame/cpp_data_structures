124e815f4b56b8ee222c9960acb534fa05df47b5
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-07 10:21:18 +0530

    runtime_update

--- pre_computation/intro.cpp
+++ pre_computation/intro.cpp
@@ -1,99 +1,41 @@
-/*
-     ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् |
-     उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात् ||
-*/
-
-/*
-Given T testcases and in each test case a number N. Print its factorial for each test case %M.
-where M = 10^9+7
-Constraints
-1 <= T <= 10^5
-1 <= N <= 10^5 
-*/
-
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
+const int M=1e9+7;
 
-#define int            long long int
-#define F              first
-#define S              second
-#define pb             push_back
-#define si             set <int>
-#define vi             vector <int>
-#define pii            pair <int, int>
-#define vpi            vector <pii>
-#define vpp            vector <pair<int, pii>>
-#define mii            map <int, int>
-#define mpi            map <pii, int>
-#define spi            set <pii>
-#define endl           "\n"
-#define sz(x)          ((int) x.size())
-#define all(p)         p.begin(), p.end()
-#define double         long double
-#define que_max        priority_queue <int>
-#define que_min        priority_queue <int, vi, greater<int>>
-#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
-#define print(a)       for(auto x : a) cout << x << " "; cout << endl
-#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
-#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
-const int M = 1e9+7;
-
-inline int power(int a, int b)
-{
-    int x = 1;
-    while (b)
-    {
-        if (b & 1) x *= a;
-        a *= a;
-        b >>= 1;
-    }
-    return x;
+void fast(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
 }
 
-template <typename Arg1>
-void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
-template <typename Arg1, typename... Args>
-void __f (const char* names, Arg1&& arg1, Args&&... args)
-{
-    const char* comma = strchr (names + 1, ',');
-    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
-}
-
-const int N = 200005;
-
-void solve() {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
+void solve(){
+    int n;
         cin>>n;
         long long fact = 1;
         for(int i=2;i<=n;i++){
             fact = (fact*i)%M;
         }
         cout<<fact<<endl;
-    }
+    
 }
 
-int32_t main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
+int main(){
 #ifndef ONLINE_JUDGE
-    freopen("input.txt",  "r",  stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("error.txt", "w",  stderr);
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
 #endif
 
-    clock_t z = clock();
+    fast();
+    int t=1;
+    cin>>t;
+    clock_t b=clock();
+    while(t--){
+        solve();
+    }
+    clock_t e=clock();
 
-    //int t = 1;
-    // cin >> t;
-    //while (t--)
-     solve();
-
-    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
-
+    cerr<<"Run time: "<<e-b<<endl;
+    
     return 0;
 }
