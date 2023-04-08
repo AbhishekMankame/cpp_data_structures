74a1dedf29588716a13fc3f1b88c1b25df02e34c
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-08 18:28:22 +0530

    simple_update

---
+++ pre_computation/prefix_sum1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define ll long long int
+const int N=1e5+10; 
+int a[N];
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+}
