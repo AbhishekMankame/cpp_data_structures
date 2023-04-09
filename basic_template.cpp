44f8a54b9ccc61803c9a736beb9fccc21e1c4f47
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-09 19:37:07 +0530

    template_update

--- basic_template.cpp
+++ basic_template.cpp
@@ -1,5 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long int
+
+ll gcd(ll a, ll b){
+    if(b==0) return a;
+    else{
+        return gcd(b,a%b);
+    }
+}
+
+ll lcm(ll a, ll b){
+    return (a*b)/gcd(a,b);
+}
 
 void fast(){
     ios_base::sync_with_stdio(0); 
