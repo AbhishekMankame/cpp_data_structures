0642d24468ce5b2d2c57d8426a6b6ef3bb3790a1
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-11 00:04:23 +0530

    code_update

--- pre_computation/l_to_r_problem.cpp
+++ pre_computation/l_to_r_problem.cpp
@@ -14,6 +14,11 @@ using namespace std;
 const int N = 1e5+10;
 int a[N];
 int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
     int n;
     cin>>n;
     for(int i=1;i<=n;i++){
