0697001d44dfe74b44feccfec8f3600eed1aa7e3
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-10 22:07:43 +0530

    simple_code_update

--- pre_computation/l_to_r_problem.cpp
+++ pre_computation/l_to_r_problem.cpp
@@ -1,3 +1,10 @@
 /*
 Given array a of N integers. Given Q queries and in each query given L and R print sum of array elements from index L to R (L,R included)
+
+Constraints
+1 <= N <= 10^5
+1 <= a[i] <= 10^9
+1 <= Q <= 10^5
+1 <= L, R <= N
+
 */
