c7495fbf80849d5dabf08780bdaebbec6e9a460c
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-11 12:05:48 +0530

    basic_code_update

--- pre_computation/two_d_array_pre.cpp
+++ pre_computation/two_d_array_pre.cpp
@@ -7,3 +7,16 @@ Constraints
 1 <= Q <= 10^5
 1 <= a,b,c,d <= N
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+}
