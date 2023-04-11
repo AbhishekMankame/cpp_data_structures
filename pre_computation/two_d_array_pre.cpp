1ef2da845003326b0d5c1ecf91b54c96fdb26d41
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-11 12:04:58 +0530

    simple_details_update

---
+++ pre_computation/two_d_array_pre.cpp
@@ -0,0 +1,9 @@
+/*
+Given 2d array a of N*N integers. Given Q queries and in each query given a, b, c and d. Print sum if square represented by (a,b) as top left point and (c,d) and bottom right point
+
+Constraints
+1 <= N <= 10^3
+1 <= a[i][j] <= 10^9
+1 <= Q <= 10^5
+1 <= a,b,c,d <= N
+*/
