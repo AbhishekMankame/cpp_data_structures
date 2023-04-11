68b578c44a9050084e11e32bd64f463ebf716a1f
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-11 22:09:06 +0530

    code_update

--- pre_computation/two_d_array_prefix_sum.cpp
+++ pre_computation/two_d_array_prefix_sum.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #define ll long long int
 const int N=1e3+10;
 int arr[N][N];
-ll pf[N];
+ll pf[N][N];
 
 int main(){
 
@@ -27,6 +27,7 @@ int main(){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             cin>>arr[i][j];
+            pf[i][j] = arr[i][j] + pf[i-1][j] + pf[i][j-1] - pf[i-1][j-1];
         }
     }
     int q;
@@ -34,14 +35,8 @@ int main(){
     while(q--){
         int a,b,c,d;
         cin>>a>>b>>c>>d;
-        ll sum=0;
-        for(int i=a;i<=c;i++){
-            for(int j=b;j<=d;j++){
-                sum+=arr[i][j];
-            }
-            
-        }
-        cout<<sum<<endl;
+        
+        cout<<pf[c][d]-pf[a-1][d]-pf[c][b-1]+pf[a-1][b-1]<<endl;
     }
 
 }
