f9543c82e4b68aa30044e7b9c30d9587e8abcdf9
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-11 10:21:05 +0530

    code_update

---
+++ pre_computation/ltor_prefix_sum.cpp
@@ -0,0 +1,29 @@
+/*Prefix sum*/
+#include<bits/stdc++.h>
+using namespace std; 
+#define ll long long int
+const int N=1e5+10; 
+int a[N];
+int pf[N];
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        pf[i]=pf[i-1] + a[i];
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        cout<<pf[r] - pf[l-1]<<endl;
+    }
+}
