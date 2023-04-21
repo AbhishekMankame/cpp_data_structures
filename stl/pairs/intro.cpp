cd17688d841c93b0a2c0d3e794ff99d2176d8888
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-21 16:35:01 +0530

    simple_update

--- stl/pairs/intro.cpp
+++ stl/pairs/intro.cpp
@@ -29,5 +29,10 @@ int main(){
     p_array[0]={1,2};
     p_array[1]={2,3};
     p_array[2]={3,4};
+
+    for(int i=0;i<3;i++){
+        cout<<p_array[i].first<<" "<<p_array[i].second<<endl;
+    }
+
     return 0;
 }
