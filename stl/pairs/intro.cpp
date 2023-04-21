00e8a98415f6be590c507c63129b0a097d010737
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-21 15:35:49 +0530

    simple_update

--- stl/pairs/intro.cpp
+++ stl/pairs/intro.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
     pair<int,string> p;
     // Adding elements or values to the pair
     // internal function --> make_pair()
