53defb653a994e8837b5cb2064d7a9bedceb78eb
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-22 10:41:50 +0530

    simple_update

--- stl/vector/2d_vector_intro.cpp
+++ stl/vector/2d_vector_intro.cpp
@@ -10,6 +10,12 @@ void fast(){
 
 void solve(){
     vector<vector<int>>arr={{1,2,3},{4,5,6},{7,8,9,10},{11,12}};
+    for(int i=0;i<arr.size();i++){
+        for(int j:arr[i]){
+            cout<<j<<endl;
+        }
+        cout<<endl;
+    }
     
     
 }
