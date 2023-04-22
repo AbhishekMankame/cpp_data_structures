3a0910837f6298064a9407f92d1d86334dfed3f7
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-22 18:24:40 +0530

    simple_update

--- stl/vector/vector_vector_sort.cpp
+++ stl/vector/vector_vector_sort.cpp
@@ -3,6 +3,10 @@
 #include<vector>
 using namespace std;
 
+int calcToMarks(vector<int> marks){
+    return marks[0]+marks[1]+marks[2];
+}
+
 bool compare(pair<string,vector<int>>s1, pair<string,vector<int>>s2){
 
     vector<int> m1 = s1.second;
