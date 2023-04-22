60a3c852e0349b03fa139defb5f6f1e471179ae9
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-22 16:56:30 +0530

    simple_update

--- stl/vector/scalar.cpp
+++ stl/vector/scalar.cpp
@@ -14,4 +14,8 @@ int main(){
     vector<int> arr ={10,11,2,3,4,6,7,8};
     int key=11;
     vector<int>::iterator it = find(arr.begin(),arr.end(),key);
+    if(it!=arr.end()){
+        cout<<it-arr.begin();
+    }
+
 }
