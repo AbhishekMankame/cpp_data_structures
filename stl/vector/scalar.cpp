f05c9caf8107051276144d623406587ce363ffb4
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-22 16:48:27 +0530

    file_update

---
+++ stl/vector/scalar.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+}
