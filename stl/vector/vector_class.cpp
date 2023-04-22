cfee148dc770b037f25c0531b81a48278eea16e3
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-22 15:59:56 +0530

    simple_update

--- stl/vector/vector_class.cpp
+++ stl/vector/vector_class.cpp
@@ -119,6 +119,13 @@ int main(){
     for(int i=0;i<v.size();i++){
         cout<<v.at(i)<<",";
     }
+    cout<<endl;
+
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<",";
+    }
+
+    cout<<endl;
 
     return 0;
 }
