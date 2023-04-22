c69914240f9693b88dccb99ae6e723a47a55b82f
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-22 17:30:18 +0530

    simple_update

---
+++ stl/vector/subsequence_search.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+
+    return 0;
+}
