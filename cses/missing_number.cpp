7dde0554750430bba559b924839767b87a2ce7ad
AbhishekMankame <abhishekmankame962@gmail.com>
2023-04-23 11:32:30 +0530

    simple_update

--- cses/missing_number.cpp
+++ cses/missing_number.cpp
@@ -11,13 +11,12 @@ int main(){
 
     ll n,s=0;
     cin>>n;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<n;i++){
         int a;
         cin>>a;
         s+=a;
     }
-    cout<<n*(n+1)/2-5<<endl;
-
+    cout<<n*(n+1)/2-s<<endl;
 
     return 0;
 }
