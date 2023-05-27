eaffcb4928afd002bd3cebdfd1464daa01d2ada4
AbhishekMankame <abhishekmankame962@gmail.com>
2023-05-27 22:22:51 +0530

    simple_update

---
+++ coding_minutes/dsa/arrays/subarray_sum_prefix_sum.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+ll gcd(ll a, ll b){
+    if(b==0) return a;
+    else{
+        return gcd(b,a%b);
+    }
+}
+
+ll lcm(ll a, ll b){
+    return (a*b)/gcd(a,b);
+}
+
+void fast(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+}
+
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+
+    fast();
+
+    return 0;
+}
