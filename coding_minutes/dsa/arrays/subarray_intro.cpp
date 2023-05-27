147caba146263018d1f44db2d349a8764d52e77f
AbhishekMankame <abhishekmankame962@gmail.com>
2023-05-27 17:32:34 +0530

    simple_update

--- coding_minutes/dsa/arrays/subarray_intro.cpp
+++ coding_minutes/dsa/arrays/subarray_intro.cpp
@@ -23,6 +23,9 @@ void printSubarray(int arr[], int n){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             //cout<<"("<<i<<","<<j<<") ";
+            for(int k=i;k<=j;k++){
+                cout<<arr[k]<<",";
+            }
         }
         cout<<endl;
     }
